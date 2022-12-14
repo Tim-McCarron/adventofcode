6a0ed0d8f670a9bc6e46108dd162e786bf7cb1a8
Tim McCarron <tim.mccarron@sonraisecurity.com>
2022-12-14 19:43:15 -0400

    fu

--- day7/index.js
+++ day7/index.js
@@ -1,14 +1,38 @@
 const fs = require('fs');
 
-
 fs.readFile('./input.txt', 'utf8', (err, data) => {
     if (err) {
         console.error(err)
         return
     }
 
-    const commands = data.split(/\r?\n/)
-    console.log(commands)
+    const directory = {}
+    let currentDir = []
+
+    const changeDirectory = command => {
+
+    }
+
+    const listDirectory = () => {
+
+    }
+
+    const commands = {
+      'cd': changeDirectory,
+      'ls': listDirectory,
+    }
+
+    const elfCommands = data.split(/\r?\n/)
+
+
+    elfCommands.forEach(command => {
+      if (command.startsWith('$')) {
+        
+      }
+    })
+
+
+    console.log(elfCommands)
 
 
 })
